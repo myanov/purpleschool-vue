4b29e87af3afa11f7ac111ec6f3759f42a7372b8
Maksim Yanov <m.yanov1998@gmail.com>
2025-08-03 14:19:48 +0700

    Домашнее задание номер 1

---
+++ 1-basics/eslint.config.js
@@ -0,0 +1,21 @@
+import vue from "eslint-plugin-vue";
+import js from "@eslint/js";
+import prettier from "eslint-config-prettier";
+
+export default [
+  js.configs.recommended,
+  ...vue.configs["flat/recommended"],
+  prettier,
+  {
+    files: ["**/*.vue", "**/*.js"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+    },
+    rules: {
+      "vue/multi-word-component-names": "off", // Отключаем требование длинных имен
+      "vue/require-default-prop": "off", // Не требовать обязательные default props
+      // "vue/v-bind-style": ["error", "shorthand"], // Это правило отвечает за замену v-bind: на :
+    },
+  },
+];
---
+++ 1-basics/src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+  let getCurrentDate = () => {
+    let date = new Date();
+    let day = date.getDate();
+    day = day < 10 ? "0" + day : day;
+    let month = date.getMonth() + 1;
+    month = month < 10 ? "0" + month : month;
+    const year = date.getFullYear();
+    return `${day}.${month}.${year}`;
+  };
+</script>
+
+<template>{{ getCurrentDate() }}</template>
+
+<style scoped></style>
